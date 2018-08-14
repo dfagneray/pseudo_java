2d36f4aa66de2942765f30c4fb72f22229f05a24
dfagneray <david.fagneray@gmail.com>
2018-08-14 18:39:57 +0200

    Start of Pseudo-Java

---
+++ test_errors.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class test_errors{ 
+	public static void main(String[] args){
+		ArrayList<String> a = new ArrayList<String>();
+		String b = "5";
+		a.add(b);
+		a.remove(b);
+	}
+}
